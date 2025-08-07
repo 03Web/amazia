6afa25812f639a0b52cd9ce57eeb74fefb6e0f90
amazia <amaziakristanto03@gmail.com>
2025-08-07 12:55:06 +0700

    update

--- js/app-core.js
+++ js/app-core.js
@@ -345,7 +345,7 @@ const App = (() => {
 
     const params = new URLSearchParams(window.location.search);
     const hasAccessKey =
-      params.get("access_key") === "nganu-amazia-kristanto";
+      params.get("access_key") === "5895732857248594725894725984579452749857498";
 
     // Logika baru yang lebih sederhana dan benar:
     // Jika PENGGUNA TIDAK LOGIN dan DIA TIDAK DI HALAMAN INDEX...
